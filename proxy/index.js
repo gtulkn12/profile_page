69bf58a71ca1862bfb5b26a538c784d02daa4dac
gtulkn12 <gtulkn1@yahoo.com>
2021-04-22 11:43:11 -0400

    initial commit

--- proxy/index.js
+++ proxy/index.js
@@ -1,14 +1,33 @@
 const { getJobs } = require("./services");
 const express = require("express");
 
-const server = express();
-server.listen(3000);
-server.get("/jobs", (req, res) => {
-  console.log("success");
+const jobs = [{ title: "Director of Ops", salary: "50000", company: "Amazon" }];
+
+const webApp = express();
+const PORT = 3000;
+webApp.listen(PORT, () => {
+  console.log(`server listen on ${PORT}`);
 });
 
-server.get("/", (req, res) => {
+//GET JOBS
+webApp.get("/jobs", (req, res) => {
+  const { tech } = req.query;
+
+  if (tech === undefined)
+    return res.status(400).send({ error: "tech query parameter is required" });
+
+  getJobs(tech).then((jobs) => res.send(jobs));
+});
+
+// webApp.get("/jobs", async (req, res) => {
+//   const { tech } = req.query;
+//   getJobs(tech).then((jobs) => res.send(jobs));
+// });
+
+//GET
+webApp.get("/", (req, res) => {
   console.log("home route");
+  res.send(getJobs("Python"));
 });
 
 // getJobs("Python").then((PythonJobs) => console.log(PythonJobs[0]));
